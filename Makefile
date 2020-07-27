d6c2cf8466d550539fca3ac9a5042100da585416
David Hayes <drhayes@gmail.com>
2020-07-26 21:47:24 -0500

    Makefile handles exporting Tiled maps.

--- Makefile
+++ Makefile
@@ -6,6 +6,7 @@ TILEMAP_ASSETS_DIR := $(ASSETS_DIR)/tilemaps
 MEDIA_DIR := media
 IMAGES_DIR := $(MEDIA_DIR)/images
 JSON_DIR := $(MEDIA_DIR)/json
+TILEMAPS_DIR := $(MEDIA_DIR)/tilemaps
 SRC_DIR := src
 TMP_DIR := tmp
 TILES_TMP := $(TMP_DIR)/tiles
@@ -25,6 +26,7 @@ TILED=/home/drhayes/bin/Tiled-1.3.3-x86_64.AppImage
 # Generated stuff.
 # Tiles.
 TILE_MARKERS=$(patsubst $(TILE_ASSETS_DIR)/%.ase,$(TILES_TMP)/%.tilemarker,$(TILE_ASSETS))
+CONVERTED_TILEMAPS=$(patsubst $(TILEMAP_ASSETS_DIR)/%.tmx,$(TILEMAPS_DIR)/%.lua,$(TILEMAP_ASSETS))
 
 
 ###############
@@ -32,11 +34,14 @@ TILE_MARKERS=$(patsubst $(TILE_ASSETS_DIR)/%.ase,$(TILES_TMP)/%.tilemarker,$(TIL
 ###############
 
 .PHONY: start
-start: $(SRC_DIR)/media $(SRC_DIR)/lib tiles media/images/icon.png
+start: $(SRC_DIR)/media $(SRC_DIR)/lib tiles media/images/icon.png tilemaps
 	@exec love $(SRC_DIR)
 
 .PHONY: tiles
 tiles: $(IMAGES_DIR)/tiles.png $(JSON_DIR)/tiles.json
+
+.PHONY: tilemaps
+tilemaps: $(CONVERTED_TILEMAPS)
 
 
 ###############
@@ -55,16 +60,11 @@ $(IMAGES_DIR):
 $(JSON_DIR):
 	mkdir -p $@
 
-$(TILES_TMP):
+$(TILEMAPS_DIR):
 	mkdir -p $@
 
-
-########
-# Misc #
-########
-
-media/images/icon.png: $(IMAGE_ASSETS_DIR)/icon.ase
-	$(ASEPRITE) --batch $< --save-as $@
+$(TILES_TMP):
+	mkdir -p $@
 
 
 #########
@@ -76,3 +76,19 @@ $(IMAGES_DIR)/tiles.png $(JSON_DIR)/tiles.json: $(TILE_MARKERS) | $(IMAGES_DIR) 
 
 $(TILES_TMP)/%.tilemarker: $(TILE_ASSETS_DIR)/%.ase | $(TILES_TMP)
 	$(ASEPRITE) --batch --split-slices $< --save-as '$(TILES_TMP)/$*-{slice}.png' && touch $@
+
+
+############
+# Tilemaps #
+############
+
+$(TILEMAPS_DIR)/%.lua: $(TILEMAP_ASSETS_DIR)/%.tmx | $(TILEMAPS_DIR)
+	$(TILED) $< --export-map $@
+
+
+########
+# Misc #
+########
+
+media/images/icon.png: $(IMAGE_ASSETS_DIR)/icon.ase
+	$(ASEPRITE) --batch $< --save-as $@
---
+++ media/tilemaps/entrance.lua
@@ -0,0 +1,541 @@
+return {
+  version = "1.2",
+  luaversion = "5.1",
+  tiledversion = "1.3.3",
+  orientation = "orthogonal",
+  renderorder = "right-down",
+  width = 100,
+  height = 100,
+  tilewidth = 16,
+  tileheight = 16,
+  nextlayerid = 3,
+  nextobjectid = 1,
+  properties = {},
+  tilesets = {
+    {
+      name = "stone",
+      firstgid = 1,
+      filename = "../../assets/tilemaps/stone.tsx",
+      tilewidth = 16,
+      tileheight = 16,
+      spacing = 0,
+      margin = 0,
+      columns = 0,
+      tileoffset = {
+        x = 0,
+        y = 0
+      },
+      grid = {
+        orientation = "orthogonal",
+        width = 1,
+        height = 1
+      },
+      properties = {},
+      terrains = {},
+      tilecount = 62,
+      tiles = {
+        {
+          id = 0,
+          image = "../../tmp/tiles/stoneTiles-bottom-left-interior-1.png",
+          width = 16,
+          height = 16
+        },
+        {
+          id = 1,
+          image = "../../tmp/tiles/stoneTiles-bottom-left-interior-2.png",
+          width = 16,
+          height = 16
+        },
+        {
+          id = 2,
+          image = "../../tmp/tiles/stoneTiles-bottom-left-knee.png",
+          width = 16,
+          height = 16
+        },
+        {
+          id = 3,
+          image = "../../tmp/tiles/stoneTiles-bottom-left-stone-1.png",
+          width = 16,
+          height = 16
+        },
+        {
+          id = 4,
+          image = "../../tmp/tiles/stoneTiles-bottom-left-stone-knee-1.png",
+          width = 16,
+          height = 16
+        },
+        {
+          id = 5,
+          image = "../../tmp/tiles/stoneTiles-bottom-left.png",
+          width = 16,
+          height = 16
+        },
+        {
+          id = 6,
+          image = "../../tmp/tiles/stoneTiles-bottom-middle-1.png",
+          width = 16,
+          height = 16
+        },
+        {
+          id = 7,
+          image = "../../tmp/tiles/stoneTiles-bottom-middle-2.png",
+          width = 16,
+          height = 16
+        },
+        {
+          id = 8,
+          image = "../../tmp/tiles/stoneTiles-bottom-middle-3.png",
+          width = 16,
+          height = 16
+        },
+        {
+          id = 9,
+          image = "../../tmp/tiles/stoneTiles-bottom-middle-interior-1.png",
+          width = 16,
+          height = 16
+        },
+        {
+          id = 10,
+          image = "../../tmp/tiles/stoneTiles-bottom-right-interior-1.png",
+          width = 16,
+          height = 16
+        },
+        {
+          id = 11,
+          image = "../../tmp/tiles/stoneTiles-bottom-right-interior-2.png",
+          width = 16,
+          height = 16
+        },
+        {
+          id = 12,
+          image = "../../tmp/tiles/stoneTiles-bottom-right-knee.png",
+          width = 16,
+          height = 16
+        },
+        {
+          id = 13,
+          image = "../../tmp/tiles/stoneTiles-bottom-right-stone-1.png",
+          width = 16,
+          height = 16
+        },
+        {
+          id = 14,
+          image = "../../tmp/tiles/stoneTiles-bottom-right-stone-knee-1.png",
+          width = 16,
+          height = 16
+        },
+        {
+          id = 15,
+          image = "../../tmp/tiles/stoneTiles-bottom-right.png",
+          width = 16,
+          height = 16
+        },
+        {
+          id = 16,
+          image = "../../tmp/tiles/stoneTiles-bottom-stone-1.png",
+          width = 16,
+          height = 16
+        },
+        {
+          id = 17,
+          image = "../../tmp/tiles/stoneTiles-bottom-stone-2.png",
+          width = 16,
+          height = 16
+        },
+        {
+          id = 18,
+          image = "../../tmp/tiles/stoneTiles-bottom-stone-3.png",
+          width = 16,
+          height = 16
+        },
+        {
+          id = 19,
+          image = "../../tmp/tiles/stoneTiles-center-1.png",
+          width = 16,
+          height = 16
+        },
+        {
+          id = 20,
+          image = "../../tmp/tiles/stoneTiles-center-2.png",
+          width = 16,
+          height = 16
+        },
+        {
+          id = 21,
+          image = "../../tmp/tiles/stoneTiles-center-3.png",
+          width = 16,
+          height = 16
+        },
+        {
+          id = 22,
+          image = "../../tmp/tiles/stoneTiles-center-4.png",
+          width = 16,
+          height = 16
+        },
+        {
+          id = 23,
+          image = "../../tmp/tiles/stoneTiles-center-5.png",
+          width = 16,
+          height = 16
+        },
+        {
+          id = 24,
+          image = "../../tmp/tiles/stoneTiles-center-6.png",
+          width = 16,
+          height = 16
+        },
+        {
+          id = 25,
+          image = "../../tmp/tiles/stoneTiles-center-stone-1.png",
+          width = 16,
+          height = 16
+        },
+        {
+          id = 26,
+          image = "../../tmp/tiles/stoneTiles-ledge-1.png",
+          width = 16,
+          height = 16
+        },
+        {
+          id = 27,
+          image = "../../tmp/tiles/stoneTiles-ledge-2.png",
+          width = 16,
+          height = 16
+        },
+        {
+          id = 28,
+          image = "../../tmp/tiles/stoneTiles-left-side-1.png",
+          width = 16,
+          height = 16
+        },
+        {
+          id = 29,
+          image = "../../tmp/tiles/stoneTiles-left-side-2.png",
+          width = 16,
+          height = 16
+        },
+        {
+          id = 30,
+          image = "../../tmp/tiles/stoneTiles-left-side-3.png",
+          width = 16,
+          height = 16
+        },
+        {
+          id = 31,
+          image = "../../tmp/tiles/stoneTiles-left-side-interior.png",
+          width = 16,
+          height = 16
+        },
+        {
+          id = 32,
+          image = "../../tmp/tiles/stoneTiles-left-side-stone-1.png",
+          width = 16,
+          height = 16
+        },
+        {
+          id = 33,
+          image = "../../tmp/tiles/stoneTiles-mud-1.png",
+          width = 16,
+          height = 16
+        },
+        {
+          id = 34,
+          image = "../../tmp/tiles/stoneTiles-right-side-1.png",
+          width = 16,
+          height = 16
+        },
+        {
+          id = 35,
+          image = "../../tmp/tiles/stoneTiles-right-side-2.png",
+          width = 16,
+          height = 16
+        },
+        {
+          id = 36,
+          image = "../../tmp/tiles/stoneTiles-right-side-3.png",
+          width = 16,
+          height = 16
+        },
+        {
+          id = 37,
+          image = "../../tmp/tiles/stoneTiles-right-side-interior.png",
+          width = 16,
+          height = 16
+        },
+        {
+          id = 38,
+          image = "../../tmp/tiles/stoneTiles-right-side-stone-1.png",
+          width = 16,
+          height = 16
+        },
+        {
+          id = 39,
+          image = "../../tmp/tiles/stoneTiles-surface-wart-1.png",
+          width = 16,
+          height = 16
+        },
+        {
+          id = 40,
+          image = "../../tmp/tiles/stoneTiles-top-left-interior-1.png",
+          width = 16,
+          height = 16
+        },
+        {
+          id = 41,
+          image = "../../tmp/tiles/stoneTiles-top-left-interior-2.png",
+          width = 16,
+          height = 16
+        },
+        {
+          id = 42,
+          image = "../../tmp/tiles/stoneTiles-top-left-knee-stone-1.png",
+          width = 16,
+          height = 16
+        },
+        {
+          id = 43,
+          image = "../../tmp/tiles/stoneTiles-top-left-knee.png",
+          width = 16,
+          height = 16
+        },
+        {
+          id = 44,
+          image = "../../tmp/tiles/stoneTiles-top-left-stone-1.png",
+          width = 16,
+          height = 16
+        },
+        {
+          id = 45,
+          image = "../../tmp/tiles/stoneTiles-top-left.png",
+          width = 16,
+          height = 16
+        },
+        {
+          id = 46,
+          image = "../../tmp/tiles/stoneTiles-top-middle-interior-1.png",
+          width = 16,
+          height = 16
+        },
+        {
+          id = 47,
+          image = "../../tmp/tiles/stoneTiles-top-middle.png",
+          width = 16,
+          height = 16
+        },
+        {
+          id = 48,
+          image = "../../tmp/tiles/stoneTiles-top-right-interior-1.png",
+          width = 16,
+          height = 16
+        },
+        {
+          id = 49,
+          image = "../../tmp/tiles/stoneTiles-top-right-interior-2.png",
+          width = 16,
+          height = 16
+        },
+        {
+          id = 50,
+          image = "../../tmp/tiles/stoneTiles-top-right-knee-stone-1.png",
+          width = 16,
+          height = 16
+        },
+        {
+          id = 51,
+          image = "../../tmp/tiles/stoneTiles-top-right-knee.png",
+          width = 16,
+          height = 16
+        },
+        {
+          id = 52,
+          image = "../../tmp/tiles/stoneTiles-top-right-stone-1.png",
+          width = 16,
+          height = 16
+        },
+        {
+          id = 53,
+          image = "../../tmp/tiles/stoneTiles-top-right.png",
+          width = 16,
+          height = 16
+        },
+        {
+          id = 54,
+          image = "../../tmp/tiles/stoneTiles-top-stone-1.png",
+          width = 16,
+          height = 16
+        },
+        {
+          id = 55,
+          image = "../../tmp/tiles/stoneTiles-top-stone-2.png",
+          width = 16,
+          height = 16
+        },
+        {
+          id = 56,
+          image = "../../tmp/tiles/stoneTiles-top-stone-3.png",
+          width = 16,
+          height = 16
+        },
+        {
+          id = 57,
+          image = "../../tmp/tiles/stoneTiles-top-stone-4.png",
+          width = 16,
+          height = 16
+        },
+        {
+          id = 58,
+          image = "../../tmp/tiles/stoneTiles-bottom-right-knee-hole.png",
+          width = 16,
+          height = 16
+        },
+        {
+          id = 59,
+          image = "../../tmp/tiles/stoneTiles-bottom-left-knee-hole.png",
+          width = 16,
+          height = 16
+        },
+        {
+          id = 60,
+          image = "../../tmp/tiles/stoneTiles-top-right-knee-hole.png",
+          width = 16,
+          height = 16
+        },
+        {
+          id = 61,
+          image = "../../tmp/tiles/stoneTiles-top-left-knee-hole.png",
+          width = 16,
+          height = 16
+        }
+      }
+    },
+    {
+      name = "collision",
+      firstgid = 63,
+      filename = "../../assets/tilemaps/collision.tsx",
+      tilewidth = 16,
+      tileheight = 16,
+      spacing = 0,
+      margin = 0,
+      columns = 0,
+      tileoffset = {
+        x = 0,
+        y = 0
+      },
+      grid = {
+        orientation = "orthogonal",
+        width = 1,
+        height = 1
+      },
+      properties = {},
+      terrains = {},
+      tilecount = 3,
+      tiles = {
+        {
+          id = 0,
+          properties = {
+            ["isSolid"] = true
+          },
+          image = "../../tmp/tiles/collisionTiles-solid.png",
+          width = 16,
+          height = 16
+        },
+        {
+          id = 1,
+          properties = {
+            ["isOneWayUp"] = true
+          },
+          image = "../../tmp/tiles/collisionTiles-one-way-up.png",
+          width = 16,
+          height = 16
+        },
+        {
+          id = 2,
+          properties = {
+            ["isHidden"] = true
+          },
+          image = "../../tmp/tiles/collisionTiles-hidden.png",
+          width = 16,
+          height = 16
+        }
+      }
+    }
+  },
+  layers = {
+    {
+      type = "tilelayer",
+      id = 2,
+      name = "floor",
+      x = 0,
+      y = 0,
+      width = 100,
+      height = 100,
+      visible = true,
+      opacity = 1,
+      offsetx = 0,
+      offsety = 0,
+      properties = {},
+      encoding = "lua",
+      chunks = {
+        {
+          x = -16, y = -16, width = 16, height = 16,
+          data = {
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 45, 57, 55, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 45, 43, 32, 25, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 4, 19, 18, 17, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            46, 48, 48, 48, 48, 48, 48, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            6, 1, 21, 20, 20, 20, 20, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 31, 20, 20, 20, 20, 20, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 6, 9, 8, 60, 20, 20, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 29, 1, 20, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 33, 3, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 4, 19, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 2147483662, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+          }
+        }
+      }
+    },
+    {
+      type = "tilelayer",
+      id = 1,
+      name = "physics",
+      x = 0,
+      y = 0,
+      width = 100,
+      height = 100,
+      visible = true,
+      opacity = 1,
+      offsetx = 0,
+      offsety = 0,
+      properties = {},
+      encoding = "lua",
+      chunks = {
+        {
+          x = -16, y = -16, width = 16, height = 16,
+          data = {
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 63, 63, 63, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            63, 63, 63, 63, 63, 63, 63, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 63, 63, 63, 63, 63, 63, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 63, 63, 63, 63, 63, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 63, 63, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 63, 63, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 63, 63, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+          }
+        }
+      }
+    }
+  }
+}
